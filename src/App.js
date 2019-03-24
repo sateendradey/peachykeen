42832819eb254224c23a2f20f275216dc9d5e869
sateendradey <satdey@Sateendras-MacBook-Pro.local>
2019-03-24 00:50:51 -0500

    humble-beginnings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './team08.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code>.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <section id= "signup">
+          <h2>Signup</h2>
+          <div class="form-group">
+            <div>
+              <input class="form-control" type="text" placeholder="Name" required />
+            </div>
+            <div>
+              <input class="form-control" type="email" placeholder="Email" required />
+            </div>
+            <div>
+              <input class="form-control" type="password" placeholder="Password" required />
+            </div>
+            <div>
+              <input class="form-control" type="password" placeholder="Repeat password" required />
+            </div>
+            <div class="terms">
+              <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+            </div>
+            <div>
+              <button type="button" class="btn btn-danger">Cancel</button>
+              <button type="submit" class="btn btn-success">Sign Up</button>
+            </div>
+            <button type="submit" class="btn btn-info">Already Have A Login?</button>
+          </div>
+        </section>
       </div>
     );
   }
