5710c6631e35e5074b6d289ff93d5add7ee1750c
sateendradey <satdey@Sateendras-MacBook-Pro.local>
2019-03-28 01:35:31 -0500

    Fix Shizz

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
-const path = require('path');.
+const path = require('path');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
