4497aa32d28bc5791ce1f4d1e2fe2b78b7e1c18f
JDWehl <49086474+JDWehl@users.noreply.github.com>
2019-03-30 22:31:26 -0500

    Update Main.js
    Yarn error on fetch ln 12

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 
 class Main extends Component {
+	
+	
+	state = {
+		restaurants: []
+	};
+	
+	 listHTML = '';
+	
+	fetch('/restaurants')
+		.then(res => res.json())
+		.then(restaurants => this.setState({ restaurants }));
+		.then(res.forEach(function(item) {
+			listHTML += '<li>' + item.name + '</li>';
+		});)
+		/*
+	document.getElementById("restaurants-list").innerHTML = '<ul>' + listHTML + '</ul>';
+	 */ 
   render() {
     return (
+	<React.Fragment>
       <div className="App">
-        Main
+		Main
       </div>
+	  <div id="restaurants-list">
+	  </div>
+	</React.Fragment>
     );
   }
 }
