26964bb7de3b4e66cef0837957f0b223cc412c2f
sateendradey <satdey@Sateendras-MacBook-Pro.local>
2019-04-19 13:38:50 -0500

    Formatting

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -48,43 +48,43 @@ class Main extends Component {
 	};
 
 	pickedRestaurant(selectedRestaurant) {
-			//selected Name
-			this.setState({detailsOpen: false});
-			const lclrest = selectedRestaurant.target.value;
-			this.setState({selectedRestaurant: lclrest});
-			//Selected Mood
-			const lclDataArray = this.state.data.filter(mood=>mood.Name == lclrest);
-			var lclMood = lclDataArray[0].Mood;
-			this.setState({restaurantMood:lclMood});
-			//Selected Rating
-			var lclRating = lclDataArray[0].Rating;
-			this.setState({restaurantRating: lclRating});
-			//Selected Reviews
-			var lclReviews = lclDataArray[0].Reviews;
-			this.setState({restaurantReviews: lclReviews});
-			//Selected City
-			var lclCity = lclDataArray[0].City;
-			this.setState({restaurantCity: lclCity});
-			//Selected state
-			var lclState = lclDataArray[0].State;
-			this.setState({restaurantState: lclState});
-			//ID
-			var lclId = lclDataArray[0]._id;
-			this.setState({restaurantId: lclId});
-			this.setState({detailsOpen: true});
-			this.setState({detailsHidden: false});
+		//selected Name
+		this.setState({detailsOpen: false});
+		const lclrest = selectedRestaurant.target.value;
+		this.setState({selectedRestaurant: lclrest});
+		//Selected Mood
+		const lclDataArray = this.state.data.filter(mood=>mood.Name == lclrest);
+		var lclMood = lclDataArray[0].Mood;
+		this.setState({restaurantMood:lclMood});
+		//Selected Rating
+		var lclRating = lclDataArray[0].Rating;
+		this.setState({restaurantRating: lclRating});
+		//Selected Reviews
+		var lclReviews = lclDataArray[0].Reviews;
+		this.setState({restaurantReviews: lclReviews});
+		//Selected City
+		var lclCity = lclDataArray[0].City;
+		this.setState({restaurantCity: lclCity});
+		//Selected state
+		var lclState = lclDataArray[0].State;
+		this.setState({restaurantState: lclState});
+		//ID
+		var lclId = lclDataArray[0]._id;
+		this.setState({restaurantId: lclId});
+		this.setState({detailsOpen: true});
+		this.setState({detailsHidden: false});
 	}
 
-  render() {
+	render() {
 		//populates the select
 		var options = this.state.restaurantNames?
-			this.state.restaurantNames.map(function(name){
-				return <option value={name}>{name}</option>
+		this.state.restaurantNames.map(function(name){
+			return <option value={name}>{name}</option>
 		})
 		: '';
 		//populates the reviews
 		var reviews = this.state.restaurantReviews?
-			this.state.restaurantReviews.map(function(review){
+		this.state.restaurantReviews.map(function(review){
 			return <li value={review}>{review}</li>
 		})
 		:'';
@@ -99,33 +99,33 @@ class Main extends Component {
 		var selectedId = this.state.restaurantId;
 
 
-    return (
-	<React.Fragment>
-	<div class = "App">
-	 <div class= "container">
-	<div class="restaurant-content">
-		<form style = {{width:'100%', textAlign: 'center'}}>
-		<h1 class="restaurant-header"> All Restaurants!</h1>
+		return (
+			<React.Fragment>
+			<div class = "App">
+			<div class= "container">
+			<div class="restaurant-content">
+			<form style = {{width:'100%', textAlign: 'center'}}>
+			<h1 class="restaurant-header"> All Restaurants!</h1>
 			<select className="form-control" id="restaurants-name" onChange={this.pickedRestaurant} style = {{width:'100%', textAlign: 'center'}}>
-				<option value="" selected>Select A Restaurant</option>{options}
+			<option value="" selected>Select A Restaurant</option>{options}
 			</select>
 
-		<details id="restaurantDeets" open={detailsOpen} value = {selectedName} hidden={detailsHidden}>
-		{selectedName}
+			<details id="restaurantDeets" open={detailsOpen} value = {selectedName} hidden={detailsHidden}>
+			{selectedName}
 			<p id="restaurantMood">{selectedMood}</p>
 			<p id="restaurantRating" >{selectedRating} Outta 5</p>
 			<p id="restaurantReviews"><strong>Reviews</strong></p>
 			<p id="restaurantId">ID: {selectedId}</p>
-				<ul id="restaurantReviewsText" style= {{listStyleType:'none'}} >{reviews}</ul>
+			<ul id="restaurantReviewsText" style= {{listStyleType:'none'}} >{reviews}</ul>
 			<address id="restaurantAddress">{selectedCity}, {selectedState}</address>
-		</details>
-		</form>
-	</div>
-	</div>
-	</div>
-	</React.Fragment>
-    );
-  }
+			</details>
+			</form>
+			</div>
+			</div>
+			</div>
+			</React.Fragment>
+		);
+	}
 }
 
 export default Main;
