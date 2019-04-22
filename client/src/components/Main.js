239296d5a6b2a75762ddb30c2f2e2c2864e150b8
sateendradey <satdey@Sateendras-MacBook-Pro.local>
2019-04-21 22:54:45 -0500

    Filter Mood

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -10,6 +10,7 @@ class Main extends Component {
 		this.componentDidMount = this.componentDidMount.bind(this);
 		this.getDataFromDb = this.getDataFromDb.bind(this);
 		this.render = this.render.bind(this);
+		this.handleChange = this.handleChange.bind(this);
 		this.state = {
 			restaurants: [],
 			selectedRestaurant: '',
@@ -24,14 +25,17 @@ class Main extends Component {
 			restaurantState:'',
 			detailsHidden:true,
 			restaurantId:'',
-			Days: []
+			Days: [],
+			Moods:[],
+			selectedMood: "-1",
+			masterData: []
 		};
 	}
 
 	//function to contain the fetch
 	componentDidMount() {
 		//sets isLoading to true if we want to display some graphics during loading
-		this.setState({ isLoading: true});
+		this.setState({ isLoading: true });
 		//fetch pulls all restaurant data into an array
 		this.getDataFromDb();
 	}
@@ -39,26 +43,39 @@ class Main extends Component {
 	getDataFromDb = async () => {
 		const response = await fetch('/restaurants');
 		const body = await response.json();
-		this.setState({data: body});
+		this.setState({data: body,
+										masterData: body});
 		var names = Array.from(body).map(names => names.Name);
-		var days = Array.from(body).map(days=>days.Days)
-		var hrs = Array.from(body).map(hrs=>hrs.Hours)
+		var days = Array.from(body).map(days=>days.Days);
+		var mood = Array.from(body).map(mood=>mood.Mood);
+		mood = Array.from(new Set(mood));
 		this.setState({
 			restaurantNames: names,
 			isLoading: false,
-			Days: days
+			Days: days,
+			Moods: mood
 		});
 	};
 
+	handleChange = async (e) =>{
+		e.preventDefault();
+    await this.setState({selectedMood: e.target.value});
+		if (this.state.selectedMood === "-1"){
+			this.setState({data: this.state.masterData})
+		}
+		else{
+			var data = this.state.masterData;
+			var newData = [];
+			data.map(restaurant => {
+				if (restaurant.Mood === this.state.selectedMood)
+					newData.push(restaurant);
+			});
+			this.setState({data: newData});			
+		}
+  }
+
 	render() {
-		//populates the select
-		var options = this.state.restaurantNames?
-		this.state.restaurantNames.map(function(name, index){
-			return <option key={index} value={name}>{name}</option>
-		})
-		: '';
 		const cards = this.state.data ? this.state.data.map(function(card){
-			console.log(card);
 			var DaysOfWeek = card.Days.map(function(day){
 				switch(day) {
 					case "M":
@@ -87,25 +104,25 @@ class Main extends Component {
 				<div className="card">
 				<div className="card-content">
 				<div className="card-body" style={{color: 'black'}}>
-				<a href={restLink}> <span class="customH4 card-title">{card.Name}</span> </a>
-				<h6 class="card-subtitle text-muted"><i className= "em em-peach" title ='Peachy Rating'></i>{card.Rating}</h6>
+				<a href={restLink}> <span className="customH4 card-title">{card.Name}</span> </a>
+				<h6 classNAme="card-subtitle text-muted"><i className= "em em-peach" title ='Peachy Rating'></i>{card.Rating}</h6>
 				</div>
 				{
 					card.Images && card.Images.length > 0 ?
-				 	<a href={restLink}><img class="img-fluid" src={"data:image/jpeg;base64,"+card.Images[0].image} alt={card.Name}/></a>
+				 	<a href={restLink}><img className="img-fluid" src={"data:image/jpeg;base64,"+card.Images[0].image} alt={card.Name}/></a>
 					: null
 			  }
 				<div className = 'card-body'>
-				<p class="card-text">
-				<i class="fas fa-grin-hearts" title="Mood"></i>  {card.Mood}
-				<i class="fas fa-utensils" title="Restaurant Type"></i>    {card.Type}
+				<p className="card-text">
+				<i className="fas fa-grin-hearts" title="Mood"></i>  {card.Mood}
+				<i className="fas fa-utensils" title="Restaurant Type"></i>    {card.Type}
 				</p>
 				<div>
 				<span className='caption' style={{width:'100%'}}>{DaysOfWeek}</span>
 				</div>
 				<span className="card-text"> {openAt} to {closeAt} </span>
 				<div className = "rightSide">
-				<a href={restLink} className="btnHeart"><i class="fas fa-heart" tite="favorite"></i></a>
+				<a href={restLink} className="btnHeart"><i className="fas fa-heart" tite="favorite"></i></a>
 				</div>
 				</div>
 				</div>
@@ -123,6 +140,17 @@ class Main extends Component {
 			loadingText="Data Loading"
 			id = "DataLoad"
 			/>
+
+			var MoodOptions = <div className="col-3" id="mooddrop">
+			<select className = "form-control" value={this.state.selectedMood} onChange={this.handleChange}>
+			<option value="-1">Anything!</option>
+			{
+				this.state.Moods.map(mood => {
+					return <option key={mood} value={mood}>{mood}</option>
+				})
+			}
+			</select>
+			</div>
 			return (
 				<React.Fragment>
 				<div className = "App">
@@ -132,6 +160,10 @@ class Main extends Component {
 				<img src={logo} alt="Peaches Logo" />
 				</div>
 				<h1> Welcome to Peachy Keen!</h1>
+				</div>
+				<div className="restaurant-header">
+				<p> What are you in the mood for? </p>
+				{MoodOptions}
 				</div>
 				<div className="row match-height">
 				{this.state.isLoading ? LoadButton : cards}
